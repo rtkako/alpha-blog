54bac70a912e5b36e199bdd07701fca7c1acf98d
Andrew K <andrew@example.com>
2016-01-22 02:32:35 +0000

    Add categories models and test

--- db/migrate/20160113042844_change_updated_at.rb
+++ db/migrate/20160113042844_change_updated_at.rb
@@ -1,5 +1,5 @@
 class ChangeUpdatedAt < ActiveRecord::Migration
-  def change
-    rename_column :articles, :udpated_at, :updated_at
-  end
+#  def change
+#    rename_column :articles, :udpated_at, :updated_at
+#  end
 end
